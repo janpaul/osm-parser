2b8e8a7528b5594a5849b97dbf8e3052c5207ecc
janpaul <janpaul@hey.com>
2023-02-02 15:06:49 +0100

    working except for relations

--- src/lib/parse.ts
+++
@@ -1,7 +0,0 @@
-import type { DenseNodes } from "../proto/osmformat_pb"
-import { MyNode } from "../types"
-
-export const parseDenseNodes = (nodes: DenseNodes): MyNode[] => {
-  console.log(nodes)
-  return []
-}
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,24 @@
-export type MyNode = {
+export type MyKeysValues = Record<string, string>
+
+export type MySimpleNode = {
+  id: number
   lat: number
   lon: number
+}
+export type MyNode = MySimpleNode & {
+  kv: MyKeysValues
+}
+// I did it...
+export type MyWay = {
+  id: number
+  refs: MySimpleNode[]
+  kv: MyKeysValues
+}
+export type MyRelationNode = MySimpleNode & {
+  role: number
+}
+export type MyRelation = {
+  id: number
+  kv: MyKeysValues
+  nodes: MyRelationNode[]
 }
